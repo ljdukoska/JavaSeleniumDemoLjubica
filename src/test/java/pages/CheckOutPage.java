e7fa42f1745a82365346fd04f85892730870e823
Ljubica Dukoska <Ljubica.Dukoska@allocatesoftware.com>
2022-09-09 02:30:33 +0200

    added checkout page

---
+++ src/test/java/pages/CheckOutPage.java
@@ -0,0 +1,52 @@
+package pages;
+
+import helpers.CommonActions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CheckOutPage extends CommonActions {
+    WebDriver driver;
+
+    public CheckOutPage(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(css = "#first-name")
+    WebElement firstName;
+    @FindBy(css = "#last-name")
+    WebElement lastName;
+    @FindBy(css = "#postal-code")
+    WebElement postalCode;
+    @FindBy(css = "#cancel")
+    WebElement cancelButton;
+    @FindBy(css = "#continue")
+    WebElement continueButton;
+
+
+    public void enterFirstName(String value){
+
+        typeText(firstName,value);
+    }
+    public void enterLastName(String value){
+
+        typeText(lastName,value);
+    }
+    public void enterPostalCode(String value){
+
+        typeText(postalCode,value);
+    }
+
+
+    public void clickCancelButton(){
+        clickElement(cancelButton);
+    }
+    public void clickContinueButton(){
+        clickElement(continueButton);
+    }
+
+
+}
